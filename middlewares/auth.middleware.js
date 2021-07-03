b8a04e5a689cf5c68d7a70d809e4856c4813c7d5
stone <stone04092000@gmail.com>
2021-07-03 07:51:28 +0700

    Add auth midddeware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const { JWT_SECRET } = require("../configs/env");
+const User = require("../models/user.model");
+
+const auth = asyncHandler(async (req, res, next) => {
+  if (req.headers && req.headers.zuthorization) {
+    const token = req.headers.authorization.split(" ")[1];
+
+    try {
+      const payload = jwt.verify(token, JWT_SECRET);
+      const user = await User.findById(payload.userId);
+
+      if (!user) {
+        res.status(404);
+        throw new Error("User not found");
+      }
+      req.user = user;
+      next();
+    } catch (err) {
+      console.error(err);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  } else {
+    // no token
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+module.exports = { auth };
