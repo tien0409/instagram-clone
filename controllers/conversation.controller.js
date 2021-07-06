dd6d5bbe74ef3073e01209dc3b6367482284d256
stone <stone04092000@gmail.com>
2021-07-06 17:31:30 +0700

    Change when create conversation

--- controllers/conversation.controller.js
+++ controllers/conversation.controller.js
@@ -2,8 +2,21 @@ const asyncHandler = require("express-async-handler");
 
 const Conversation = require("../models/conversation.model");
 
+/*
+ * @desc create conversation, if conversation exist then response conversation exist
+ * @route POST /api/conversation/:userId
+ * @access Private
+ */
 const createConversation = asyncHandler(async (req, res) => {
   const { userReq, user } = req;
+
+  const conversationExist = await Conversation.findOne({
+    members: [userReq._id, user._id],
+  });
+
+  if (conversationExist) {
+    return res.status(200).json({ _id: conversationExist._id });
+  }
 
   const conversation = new Conversation({ members: [userReq._id, user._id] });
   await conversation.save();
