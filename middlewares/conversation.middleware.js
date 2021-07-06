2c50699ad346796d3d21cd78115088f8fd827640
stone <stone04092000@gmail.com>
2021-07-06 18:42:39 +0700

    Add conversation middleware, validator conversationId

---
+++ middlewares/conversation.middleware.js
@@ -0,0 +1,19 @@
+const asyncHandler = require("express-async-handler");
+
+const Conversation = require("../models/post.model");
+
+const getConversationId = asyncHandler(
+  async (req, res, next, conversationId) => {
+    if (conversationId.match(/^[0-9a-fA-F]{24}$/)) {
+      const conversationReq = await Conversation.findById(conversationId);
+      if (conversationReq) {
+        req.conversationReq = conversationReq;
+        return next();
+      }
+    }
+    res.status(404);
+    next(new Error("Conversation not found"));
+  },
+);
+
+module.exports = { getConversationId };
