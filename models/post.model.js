1fdb06bb6660683c626de9b24d8c56a7f0e759de
stone <stone04092000@gmail.com>
2021-07-08 11:14:31 +0700

    Add field for comment schema and add timestamps for post modal

--- models/post.model.js
+++ models/post.model.js
@@ -1,58 +1,68 @@
 const mongoose = require("mongoose");
 
-const commentSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-});
-
-const postSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-    required: true,
-  },
-  imagePost: {
-    type: String,
-    required: true,
-  },
-  caption: {
-    type: String,
-  },
-  likes: [
-    {
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "user",
+      required: true,
     },
-  ],
-  comments: [commentSchema],
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    name: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-});
+  { timestamps: true },
+);
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    imagePost: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    comments: [commentSchema],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true },
+);
 
 const Post = mongoose.model("post", postSchema);
 
