f5ce5505706f9e6e030b2e1c4836f6b82bd3dc9e
stone <stone04092000@gmail.com>
2021-07-16 22:42:08 +0700

    Add field created in message modal

--- models/message.model.js
+++ models/message.model.js
@@ -19,6 +19,10 @@ const messageSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
   unread: {
     type: Boolean,
     default: true,
