d775e21de46efbb7f80a360594e092f47def77e7
stone <stone04092000@gmail.com>
2021-07-19 07:36:27 +0700

    Test

--- server.js
+++ server.js
@@ -12,14 +12,6 @@ const connectDb = require("./utils/db");
 const app = express();
 connectDb();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   const path = require("path");
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
-
 // middlewares
 app.use(express.static("public"));
 app.use(express.json());
@@ -41,6 +33,14 @@ app.use("/api/message", messageRoute);
 // handle error
 app.use(notFoundRoute);
 app.use(errHandler);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const server = app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
