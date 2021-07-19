6963426873197403685c6853cfd3f8b5e03dbd4d
stone <stone04092000@gmail.com>
2021-07-19 10:38:31 +0700

    Revert "Change get last message when first load page"
    This reverts commit 03ca0833bf91a9d5646f60e6840d21953b06a67e.

--- socket/message/index.js
+++ socket/message/index.js
@@ -44,8 +44,7 @@ module.exports = function (socket, io) {
       conversation: conversationId,
     }).sort({ createdAt: -1 });
 
-    socket.emit("server-send-last-message", lastMessage);
-    socket.to(room).emit("server-send-last-message", lastMessage);
+    io.to(room).emit("server-send-last-message", lastMessage);
   };
 
   const getNumberUnreadMessage = async () => {
