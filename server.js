a84000530e2925135988aec5dd877dc2d30b1f62
stone <stone04092000@gmail.com>
2021-07-19 07:32:13 +0700

    Test

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const connectDb = require("./utils/db");
 const app = express();
 connectDb();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 // middlewares
 app.use(express.static("public"));
