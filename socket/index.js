f3223a2640fbe58a83d3be19586cc758cfdb333e
stone <stone04092000@gmail.com>
2021-07-23 21:50:59 +0700

    Remove url socket from server

--- socket/index.js
+++ socket/index.js
@@ -7,7 +7,7 @@ const postSocket = require("./post");
 const onlineUsers = [];
 
 const socketio = (httpServer) => {
-  const io = require("socket.io")(httpServer, {cors: CLIENT_URL});
+  const io = require("socket.io")(httpServer);
 
   io.on("connection", (socket) => {
     console.log("user connect:", socket.id);
